6588fad81723a8aa61b7d8a0a8b82f18c80f3194
PlushkaNet <187215446+PlushkaNet@users.noreply.github.com>
2026-07-02 19:18:02 +0300

    add tests (single test for int)

---
+++ tests.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <chrono>
+
+#include "quicksort.hpp"
+
+template <typename T>
+T *generate_large_data(int len) {
+    T *arr = new T[len];
+    for(int i = 0; i < len; i++) {
+        arr[i] = std::rand();
+    }
+    return arr;
+}
+
+template <typename T>
+bool run_test_only(T *data, int len) {
+    auto start = std::chrono::high_resolution_clock::now();
+    qs::quicksort<T>(data, len);
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout << "sorting complete in " << (stop-start).count() / 1000000 << " ms (started at: " <<
+        start.time_since_epoch().count() << ", ended at: " << stop.time_since_epoch().count() << ")\n";
+    return qs::is_sorted(data, len);
+}
+
+template <typename T>
+bool run_test(int len) {
+    T *data = generate_large_data<T>(len);
+    std::cout << "data generated\n";
+    bool status = run_test_only<T>(data, len);
+    delete[] data;
+    return status;
+}
+
+int main() {
+    enum {ARR_LENGTH = 10000000};
+    std::cout << "testing quicksort for 10 million items\n";
+    if(!run_test<int>(ARR_LENGTH)) return 1;
+    std::cout << "test quicksort_i_s passed\n";
+}
