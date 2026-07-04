815fa93c5d8acd0ac5946793c52ebca68e61d18f
PlushkaNet <187215446+PlushkaNet@users.noreply.github.com>
2026-07-04 18:41:26 +0300

    refactor: fix comparison warnings

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,7 +8,7 @@
 #include "../utils/pretty_utils.hpp"
 
 template <typename T>
-bool run_test_only(T *data, int len) {
+bool run_test_only(T *data, unsigned int len) {
     auto start = std::chrono::high_resolution_clock::now();
     qs::quicksort<T>(data, len);
     auto stop = std::chrono::high_resolution_clock::now();
@@ -18,7 +18,7 @@ bool run_test_only(T *data, int len) {
 }
 
 template <typename T>
-bool run_test(int len, std::function<T(void)> rng) {
+bool run_test(unsigned int len, std::function<T(void)> rng) {
     T *data = generate_large_data<T>(len, rng);
     std::cout << "data generated\n";
     bool status = run_test_only<T>(data, len);
@@ -27,7 +27,7 @@ bool run_test(int len, std::function<T(void)> rng) {
 }
 
 int main() {
-    enum {ARR_LENGTH = 10000000};
+    const unsigned int ARR_LENGTH = 10000000;
 
     std::cout << "testing quicksort for 10 million items\n";
 
--- utils/pretty_utils.hpp
+++ utils/pretty_utils.hpp
@@ -3,8 +3,8 @@
 
 template <typename T>
 void print_arr(T *arr, unsigned int len) {
-    int last_index = len-1;
-    for(int i = 0; i < len; i++) {
+    unsigned int last_index = len-1;
+    for(unsigned int i = 0; i < len; i++) {
         std::cout << arr[i];
         if(i != last_index) std::cout << ", ";
     }
@@ -12,9 +12,9 @@ void print_arr(T *arr, unsigned int len) {
 }
 
 template <typename T>
-T *generate_large_data(int len, std::function<T(void)> rng) {
+T *generate_large_data(unsigned int len, std::function<T(void)> rng) {
     T *arr = new T[len];
-    for(int i = 0; i < len; i++) {
+    for(unsigned int i = 0; i < len; i++) {
         arr[i] = rng();
     }
     return arr;
